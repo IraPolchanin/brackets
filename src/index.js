2756c6a9d8a96e721c151fc9cc787cc92d5e72c7
Iryna Polchaninova <ira.polchanin@gmail.com>
2023-02-03 12:31:17 +0200

    feaf: solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  while (str.length) {
+    const len = str.length;
+
+    bracketsConfig.map(el => {
+      str = str.replace((el[0] + el[1]), '');
+    });
+
+    if (str.length == len) return false;
+  }
+
+  return true;
 }
